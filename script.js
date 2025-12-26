97de3c86034deff2fa7b55ef6348a7d44ad8e1b2
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-26 13:18:02 +0200

    Rename addSale function to addProduct

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let total = 0;
 
-function addSale() {
-  const price = document.getElementById("price").value;
-  total += Number(price);
+function addProduct() {
+  const product = document.getElementById("product");
+  const price = Number(product.value);
+
+  total += price;
   document.getElementById("total").innerText = total;
-  document.getElementById("price").value = "";
 }
